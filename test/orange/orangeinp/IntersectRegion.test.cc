01de3c1db788257ca2a20bb6960a8fa58ca8df18
Philippe Canal <pcanal@fnal.gov>
2025-10-15 16:42:53 +0000

    Test why gcov does not like EXPECT_EQ with Real3{0, 0, 0}

--- test/orange/orangeinp/IntersectRegion.test.cc
+++ test/orange/orangeinp/IntersectRegion.test.cc
@@ -2524,6 +2524,7 @@ TEST_F(TetTest, standard)
 
     // Test senses
     // clang-format off
+    int my_own_marker = 0; // To find in the .E file
     EXPECT_EQ(SignedSense::inside,
               this->calc_sense(result.node_id, Real3{0, 0, 0}));
     // a second time
@@ -2538,7 +2539,10 @@ TEST_F(TetTest, standard)
               this->calc_sense(result.node_id, Real3{0, 0, 0.1}));
 
 
-   switch (0) case 0: default: if (const ::testing::AssertionResult gtest_ar = (::testing::internal::EqHelper::Compare(
+   switch (0)
+      case 0:
+      default:
+        if (const ::testing::AssertionResult gtest_ar = (::testing::internal::EqHelper::Compare(
    "SignedSense::inside"
    ,
    "Manually added this->calc_sense(result.node_id, Real3{0, 0, 0})"
@@ -2546,11 +2550,11 @@ TEST_F(TetTest, standard)
    SignedSense::inside
    ,
    this->calc_sense(result.node_id, Real3{0, 0, 0})
-   ))) ; else ::testing::internal::AssertHelper(::testing::TestPartResult::kNonFatalFailure, "/home/pcanal/geant/sources/celeritas/test/orange/orangeinp/IntersectRegion.test.cc",
-      2549
-     , gtest_ar.failure_message()) = ::testing::Message()
-
-                                                               ;
+   ))) ;
+        else
+           ::testing::internal::AssertHelper(::testing::TestPartResult::kNonFatalFailure, "/home/pcanal/geant/sources/celeritas/test/orange/orangeinp/IntersectRegion.test.cc",
+                        2549
+                        , gtest_ar.failure_message()) = ::testing::Message();
 
     // clang-format on
 
