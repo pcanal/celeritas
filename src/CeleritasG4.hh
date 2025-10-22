3e915188fc062d27f81625f9b8b92dea56df56b8
Philippe Canal <pcanal@fnal.gov>
2025-10-22 22:54:31 +0000

    DO NOT MERGE, silly stuff

--- src/CeleritasG4.hh
+++ src/CeleritasG4.hh
@@ -1,3 +1,4 @@
+int oups(    ) {return 0;}
 //------------------------------- -*- C++ -*- -------------------------------//
 // Copyright Celeritas contributors: see top-level COPYRIGHT file for details
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
