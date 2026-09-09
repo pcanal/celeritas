7e5c4550959f16305ac4af85f4c879cb26f4871f
Philippe Canal <pcanal@fnal.gov>
2026-09-09 13:17:31 -0500

    Extend clang-tidy to all files

--- scripts/ci/clang-tidy-changed.sh
+++ scripts/ci/clang-tidy-changed.sh
@@ -35,6 +35,7 @@ git fetch --depth 1 "${REMOTE}" "${BASE_SHA}"
 # NOTE: this only compares source/app code files that have changed, and does
 # not process changes to headers.
 ALL_FILES=$(git diff --name-only --diff-filter=ACM "$BASE_SHA"..."$HEAD_SHA")
+ALL_FILES=$(find . -type f -name '*.cc' -o -name '*.hh' | sed 's|^\./||' | sort | uniq)
 CC_FILES=$(grep -E '^(src|app|test)/.*\.cc$' - <<< "$ALL_FILES") || {
   log info "No *.cc files have changed."
   exit 0
